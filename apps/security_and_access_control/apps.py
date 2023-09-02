731e400ad204ce792ed7c4058f96150ba6c731b7
StefanGlova <glovastefan@protonmail.com>
2023-09-02 12:00:14 +0100

    new apps added, some update. Core structure updated

--- apps/security_and_access_control/apps.py
+++ apps/security_and_access_control/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class UsersConfig(AppConfig):
+class SecurityAndAccessControlConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'users'
+    name = 'security_and_access_control'
