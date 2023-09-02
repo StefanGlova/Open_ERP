b9527a905c60221b486ecd7060ee4d6165af3ad4
StefanGlova <glovastefan@protonmail.com>
2023-09-02 10:24:18 +0100

    start apps created for accounting_and_finance_management, business_intelligence, customer_relationship_management, human_resources, inventory_management, users, warehouse_management

---
+++ apps/accounting_and_finance_management/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AccountingAndFinanceManagementConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'accounting_and_finance_management'
---
+++ apps/manufacturing_and_logistics_management/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ManufacturingAndLogisticsManagementConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'manufacturing_and_logistics_management'
