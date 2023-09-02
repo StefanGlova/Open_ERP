fc8f66f177f08b8db7edbcf870b7fb7b32486d52
StefanGlova <glovastefan@protonmail.com>
2023-09-02 12:28:49 +0100

    project renemand and refactored to Open_ERP

--- Open_ERP/asgi.py
+++ Open_ERP/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for project_simple_erp project.
+ASGI config for Open_ERP project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_simple_erp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Open_ERP.settings')
 
 application = get_asgi_application()
--- Open_ERP/wsgi.py
+++ Open_ERP/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for project_simple_erp project.
+WSGI config for Open_ERP project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_simple_erp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Open_ERP.settings')
 
 application = get_wsgi_application()
