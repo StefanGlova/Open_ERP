942a233bc26fc86545bf5a10bbb5b0da889ee691
StefanGlova <glovastefan@protonmail.com>
2023-09-08 07:36:07 +0100

    app accounting_and_finance_management added to settings.py for the project

--- Open_ERP/settings.py
+++ Open_ERP/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps/accounting_and_finance_management',
 ]
 
 MIDDLEWARE = [
