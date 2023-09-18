bbf273374d5310d6443c042078b03e2924c6b694
StefanGlova <glovastefan@protonmail.com>
2023-09-18 15:02:10 +0100

    structure of project updated to reflect new structure proposal

--- applications/KPIs/finance_KPIs/apps.py
+++ applications/KPIs/finance_KPIs/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class OperationConfig(AppConfig):
+class FinanceKpisConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'operation'
+    name = 'finance_KPIs'
---
+++ applications/people/disciplinary_grievance/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DisciplinaryGrievanceConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'disciplinary_grievance'
