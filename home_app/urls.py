a3c6a4f6227df8c173a875022d677f6eee86b559
StefanGlova <glovastefan@protonmail.com>
2023-09-20 20:13:29 +0100

    initial look and images for each app - all in home_app

--- home_app/urls.py
+++ home_app/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from home_app import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('people/', views.people, name='people'),
+    path('operation/', views.operation, name='operation'),
+    path('customers/', views.customers, name='customers'),
+    path('finance/', views.finance, name='finance'),
+    path('projects/', views.projects, name='projects'),
+    path('KPIs/', views.KPIs, name='KPIs')
 ]
--- home_app/views.py
+++ home_app/views.py
@@ -3,3 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home_app/home.html')
+
+def people(request):
+    return render(request, 'home_app/people.html')
+
+def operation(request):
+    return render(request, 'home_app/operation.html')
+
+def customers(request):
+    return render(request, 'home_app/customers.html')
+
+def finance(request):
+    return render(request, 'home_app/finance.html')
+
+def projects(request):
+    return render(request, 'home_app/projects.html')
+
+def KPIs(request):
+    return render(request, 'home_app/KPIs.html')
