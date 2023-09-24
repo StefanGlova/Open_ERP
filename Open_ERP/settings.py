e6ae998a9fb6cf1467558e3bb76a45766c95933a
StefanGlova <glovastefan@protonmail.com>
2023-09-24 23:06:39 +0100

    settings.py changed to get ready for deployment, staticfiles folder created and all static files collected and stored in it

--- Open_ERP/settings.py
+++ Open_ERP/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lui$lz3jbo&(04)xg=so7!008&*&%9j&v0)_daoenk!$wk(sz_'
+SECRET_KEY = os.getenv('SECRET_KEY','django-insecure-lui$lz3jbo&(04)xg=so7!008&*&%9j&v0)_daoenk!$wk(sz_')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('IS_DEVELOPMENT', True)
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [os.getenv('APP_HOST'), '127.0.0.1']
 
 
 # Application definition
@@ -136,6 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
