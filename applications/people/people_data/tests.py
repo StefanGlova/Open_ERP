6e19169106a9d17bf5931b7e375b80eaa3de5f6a
StefanGlova <glovastefan@protonmail.com>
2023-10-08 00:56:04 +0100

    tests added for both apps which have some code home_app SimpleTestCase and people_data app TestCase and TransactionTestCase

--- applications/people/people_data/tests.py
+++ applications/people/people_data/tests.py
@@ -1,3 +1,47 @@
-from django.test import TestCase
+from django.test import TestCase, TransactionTestCase
+from django.urls import reverse
+from .models import Employee
 
 # Create your tests here.
+class PeopleDataTests(TestCase):
+    def test_people_app_home(self):
+        url = reverse("people_data_home")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_people_app_add(self):
+        url = reverse("add")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+class DatabasePeopleDataTest(TransactionTestCase):
+    def setUp(self):
+        # Create an Employee object for testing
+        self.employee = Employee.objects.create(
+            first_name='John',
+            last_name='Doe',
+            address='123 Main St',
+            phone='1234567890',
+            email='john@example.com',
+            emergency_contact='Jane Doe',
+            employment_start_date='2023-01-01',
+        )
+
+    def tearDown(self):
+        # Clean up the Employee object created during the test
+        self.employee.delete()
+
+    def test_people_app_edit(self):
+        url = reverse("edit", kwargs={'id': self.employee.id})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_people_app_view_employee(self):
+        url = reverse("edit", kwargs={'id': self.employee.id})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_people_app_deleteemployee(self):
+        url = reverse("delete", kwargs={'id': self.employee.id})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
--- home_app/tests.py
+++ home_app/tests.py
@@ -1,3 +1,35 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
 
 # Create your tests here.
+class SimpleTests(SimpleTestCase):
+    def test_home_app_home(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_app_operation(self):
+        response = self.client.get("/operation/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_app_customers(self):
+        response = self.client.get("/customers/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_app_finance(self):
+        response = self.client.get("/finance/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_app_projects(self):
+        response = self.client.get("/projects/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_app_KPIs(self):
+        response = self.client.get("/KPIs/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_app_about(self):
+        response = self.client.get("/about/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_app_people(self):
+        response = self.client.get("/people/")
+        self.assertEqual(response.status_code, 200)
