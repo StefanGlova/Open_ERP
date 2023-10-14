60187ab69bcfe9ffc1c60891404c463ba2acb8f3
StefanGlova <glovastefan@protonmail.com>
2023-10-14 19:18:34 +0100

    urls.py in people_data app updated

--- applications/people/people_data/urls.py
+++ applications/people/people_data/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.people_data_home, name='people_data_home'),
-    path('<int:id>', views.view_employee, name='view_employee'),
-    path('add/', views.add, name='add'),
-    path('edit/<int:id>', views.edit, name='edit'),
-    path('delete/<int:id>', views.delete, name='delete')
+    path('people_data/', views.people_data_home, name='people_data_home'),
+    path('people_data/<int:id>', views.view_employee, name='view_employee'),
+    path('people_data/add/', views.add, name='add'),
+    path('people_data/edit/<int:id>', views.edit, name='edit'),
+    path('people_data/delete/<int:id>', views.delete, name='delete')
 ]
