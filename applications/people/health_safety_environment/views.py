9ee59fcfd2f3111671b335f55b81edca9371abc6
StefanGlova <glovastefan@protonmail.com>
2023-10-14 19:07:44 +0100

    views.py finished in health safety and environmnent app, next step urls.py and debugging.

--- applications/people/health_safety_environment/views.py
+++ applications/people/health_safety_environment/views.py
@@ -41,13 +41,65 @@ class AddIncidentView(View):
             "description": request.POST.get("description"),
         }
 
-        response = requests.post("https://safetyapi-open-erp.onrender.com/api/incidents", json=data)
+        response = requests.post(
+            "https://safetyapi-open-erp.onrender.com/api/incidents", json=data
+        )
 
         if response.status_code == 201:
             return JsonResponse({"message": "Incident added successfully"}, status=201)
         else:
-            return JsonResponse({"error": "Incident report failed. Check your data"}, status=400)
-        
+            return JsonResponse(
+                {"error": "Incident report failed. Check your data"}, status=400
+            )
 
 
+class EditIncidentView(View):
+    def get(self, request, incident_id):
+        response = requests.get(
+            f"https://safetyapi-open-erp.onrender.com/api/incidents/{incident_id}"
+        )
+        if response.status_code == 200:
+            incident = response.json()
+            return render(request, "edit_incident.html", {"incident": incident})
+        else:
+            return JsonResponse({"error": "Incident not found"}, status=404)
 
+    def post(self, request, incident_id):
+        data = {
+            "incident_type": request.POST.get("incident_type"),
+            "person_name": request.POST.get("person_name"),
+            "incident_date": request.POST.get("incident_date"),
+            "incident_time": request.POST.get("incident_time"),
+            "witness_name": request.POST.get("witness_name"),
+            "weather": request.POST.get("weather"),
+            "light": request.POST.get("light"),
+            "description": request.POST.get("description"),
+        }
+
+        response = requests.put(
+            f"https://safetyapi-open-erp.onrender.com/api/incidents/{incident_id}",
+            json=data,
+        )
+
+        if response.status_code == 201:
+            return JsonResponse(
+                {"message": "Incident updated successfully"}, status=201
+            )
+        else:
+            return JsonResponse(
+                {"error": "Incident update failed. Check your data"}, status=400
+            )
+
+
+class DeleteIncidentView(View):
+    def post(self, request, incident_id):
+        response = requests.delete(
+            f"https://safetyapi-open-erp.onrender.com/api/incidents/{incident_id}"
+        )
+
+        if response.status_code == 201:
+            return JsonResponse(
+                {"message": "Incident deleted successfully"}, status=201
+            )
+        else:
+            return JsonResponse({"error": "Incident deletion failed"}, status=400)
