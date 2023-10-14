c688021566f776392f012890572a91225be92fbd
StefanGlova <glovastefan@protonmail.com>
2023-10-14 19:13:39 +0100

    urls.py created, next step creating necessary html files and testing

--- applications/people/health_safety_environment/urls.py
+++ applications/people/health_safety_environment/urls.py
@@ -1,1 +1,10 @@
+from django.urls import path
+from .views import ListIncidentsView, ViewIncidentView, AddIncidentView, EditIncidentView, DeleteIncidentView
 
+urlpatterns = [
+    path("incidents/", ListIncidentsView.as_view(), name="list_incidents"),
+    path("incidents/<int:incident_id>/", ViewIncidentView.as_view(), name="view_incident"),
+    path("incidents/add/", AddIncidentView.as_view(), name="add_incident"),
+    path("incidents/<int:incident_id>/edit/", EditIncidentView.as_view(), name="edit_incident"),
+    path("incidents/<int:incident_id>/delete/", DeleteIncidentView.as_view(), name="delete_incident"),
+]
